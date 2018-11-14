3b236bcb6aa05e0d8a961801937186e61ff065b9
skruer <sean.kruer@gmail.com>
2018-11-14 15:11:59 -0600

    Changing planet spawning logic
    Only spawn new planets when the boss planet is complete.

--- Terrafold/classes/Space.js
+++ Terrafold/classes/Space.js
@@ -5,15 +5,14 @@ function Space() {
     this.tick = function() {
         for(var i = 0; i < this.planets.length; i++) {
             this.planets[i].tick();
-            if(this.planets[i].isBoss && !this.planets[i].alive())
-            {
-                this.sector++;
-                this.newLevel();
-                this.planets[i].isBoss = false;
-            }
         }
         for(i = 0; i < this.ships.length; i++) {
             this.ships[i].tick();
+        }
+        // Intentionally repeat this until we have space to add new planets.
+        if(this.planets[this.planets.length - 1].doneBuilding())
+        {
+            this.newLevel();
         }
         this.planets = this.planets.filter(function(value, index, arr) { return !value.empty() })
     };
@@ -25,18 +24,25 @@ function Space() {
     };
 
     this.calcDifficulty = function() {
-        return this.sector + 1; //should this be more complicated ?
+        return this.sector > 0 ? this.sector : 1; //should this be more complicated ?
     };
 
     this.newLevel = function() {
-        for(var i = 0; i < 10; i++) {
-            this.planets.push(new Planet());
-        }
-        sortArrayObjectsByValue(this.planets, "x");
-        this.planets[this.planets.length - 1].isBoss = true; //rightmost planet
+        // Add a maximum of 40 planets for now? Should we wait until the boss planet is empty before spawning more?
+        if(this.planets.length <= 30)
+        {
+            this.sector++;
+            newPlanets = [];
+            for(var i = 0; i < 10; i++) {
+                newPlanets.push(new Planet());
+            }
+            sortArrayObjectsByValue(newPlanets, "x");
+            newPlanets[newPlanets.length - 1].isBoss = true; //rightmost planet
 
-        for(i = 0; i < this.planets.length; i++) {
-            this.planets[i].calcPower(i + this.sector * 10, this.calcDifficulty());
+            for(i = 0; i < newPlanets.length; i++) {
+                newPlanets[i].calcPower(i + this.sector * 10, this.calcDifficulty());
+            }
+            this.planets.push(newPlanets);
         }
     };
 }
